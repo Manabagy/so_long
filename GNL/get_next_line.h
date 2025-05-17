dfc7430a3946d2a52608157015215ad8c242e5a5
Manana Baghdasaryan <mabaghda@c1r1s1.42yerevan.am>
2025-05-17 14:07:20 +0400

    added gnl

--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -1,28 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mabaghda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/14 19:17:11 by mabaghda          #+#    #+#             */
-/*   Updated: 2025/05/17 13:34:39 by mabaghda         ###   ########.fr       */
+/*   Created: 2025/02/26 13:03:31 by mabaghda          #+#    #+#             */
+/*   Updated: 2025/03/04 13:16:49 by mabaghda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-# include "mlx.h"
+# include <stdlib.h>
+# include <unistd.h>
 
-typedef struct s_game
-{
-	char	**map;
-	int		width;
-	int		height;
-	int		player_x;
-	int		player_y;
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
-}			t_game;
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+int		ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
--- main.c
+++ main.c
@@ -6,8 +6,19 @@
 /*   By: mabaghda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 19:19:28 by mabaghda          #+#    #+#             */
-/*   Updated: 2025/05/14 19:19:48 by mabaghda         ###   ########.fr       */
+/*   Updated: 2025/05/17 12:22:42 by mabaghda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+int	main(void)
+{
+	void *mlx;
+	void *win;
+
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, width, height, "so_long");
+	
+	return (0);
+}
