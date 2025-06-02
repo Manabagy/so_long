51faec02ac080eb06df7adead3092ec873d4be51
Manana Baghdasaryan <mabaghda@c1r1s1.42yerevan.am>
2025-06-02 14:27:15 +0400

    instead of redrawing map I redraw changed tiles

--- src/key_handling.c
+++ src/key_handling.c
@@ -6,7 +6,7 @@
 /*   By: mabaghda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 19:27:59 by manana            #+#    #+#             */
-/*   Updated: 2025/06/02 14:15:50 by mabaghda         ###   ########.fr       */
+/*   Updated: 2025/06/02 14:26:30 by mabaghda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,9 +53,10 @@ void	move_player(t_game *data, int move_y, int move_x)
 			return ;
 	}
 	data->moves++;
-	// data->map[data->player.pos_y][data->player.pos_x] = '0';
+	ft_printf("%d\n", data->collected);
+	data->map[data->player.pos_y][data->player.pos_x] = '0';
+	data->map[new_pos_y][new_pos_x] = 'P';
 	draw_image(data, data->img_floor, data->player.pos_y, data->player.pos_x);
-	// data->map[new_pos_y][new_pos_x] = 'P';
 	draw_image(data, data->img_player, new_pos_y, new_pos_x);
 	data->player.pos_x = new_pos_x;
 	data->player.pos_y = new_pos_y;
